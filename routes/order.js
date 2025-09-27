fe323f8123fd993e8dd1b3280c07d462b1bdf83b
bairariyakhushal <bairariyakhushal@gmail.com>
2025-09-27 11:05:44 +0530

    updated get vendor and customer routes

--- routes/order.js
+++ routes/order.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const {
     createPaymentOrder,
     verifyPayment,
@@ -8,22 +9,26 @@ const {
     getVendorOrders,
     updateOrderStatus,
     cancelOrder,
-    trackOrder
+    trackOrder,
+    verifyPrescription
 } = require("../controllers/order");
-const { auth, isVendor } = require("../middlewares/auth");
+const { auth, isVendor,isCustomer } = require("../middlewares/auth");
 
 // Payment & Order routes
-router.post("/create-payment-order", auth, createPaymentOrder);
+router.post("/create-payment-order", auth,isCustomer,createPaymentOrder);
 router.post("/verify-payment", auth, verifyPayment);
 
 // Customer routes
-router.get("/my-orders", auth, getUserOrders);
+router.get("/my-orders", auth, isCustomer,getUserOrders);
+router.get("/my-orders/:status", auth, isCustomer,getUserOrders);
 router.get("/:orderId", auth, getOrderDetails);
 router.put("/cancel/:orderId", auth, cancelOrder);
 router.get("/track/:orderId", auth, trackOrder);
 
 // Vendor routes
 router.get("/vendor/orders", auth, isVendor, getVendorOrders);
+router.get("/vendor/orders/:status", auth, isVendor, getVendorOrders); // Status-wise orders
 router.put("/vendor/status/:orderId", auth, isVendor, updateOrderStatus);
+router.put("/verify-prescription/:orderId", auth, isVendor, verifyPrescription);
 
 module.exports = router;
